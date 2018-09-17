4a8d8361f0057256af4182d089a1766e6ebf342a
MortenMC-easj <31882401+MortenMC-easj@users.noreply.github.com>
2018-09-17 10:54:52 +0200

    Hello

---
+++ MyMathDLL/MyMathDLLTest/MathTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMathDLL;
+
+namespace MyMathDLLTest
+{
+    [TestClass]
+    public class MathTest
+    {
+        MyMath m = new MyMath();
+        [TestMethod]
+        public void addTest()
+        {
+
+            Assert.IsTrue(m.Add(2, 3) == 5);
+        }
+
+        [TestMethod]
+
+        public void addTestNegativeNumbers()
+        {
+            Assert.AreEqual(m.Add(-3, -4), -7);
+        }
+    }
+}
---
+++ MyMathDLL/MyMathDLLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyMathDLLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyMathDLLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6b7afacd-2305-4740-a7c7-1f9638798e35")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
